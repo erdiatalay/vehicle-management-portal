ce0bb15ba7792205f5b4f355b2ad0d33f8c83e76
Erdi Atalay <erdi.atly@gmail.com>
2019-09-15 14:27:40 +0300

    All needed component and services are created

--- vehiclemanagement-client/src/app/app.module.ts
+++ vehiclemanagement-client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateVehicleComponent } from './components/create-vehicle/create-vehicle.component';
+import { ListVehicleComponent } from './components/list-vehicle/list-vehicle.component';
+import { ViewVehicleComponent } from './components/view-vehicle/view-vehicle.component';
+import { UpdateVehicleComponent } from './components/update-vehicle/update-vehicle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateVehicleComponent,
+    ListVehicleComponent,
+    ViewVehicleComponent,
+    UpdateVehicleComponent
   ],
   imports: [
     BrowserModule,
